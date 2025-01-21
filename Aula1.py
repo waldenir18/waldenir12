1a8c911b4a4e7731719ff44a0f10b9d5f863e20b
waldenir18 <waldenireves@gmail.com>
2025-01-21 16:34:20 -0400

    commit inicial

---
+++ Aula1.py
@@ -0,0 +1,50 @@
+################ AULA 2 #################
+n1 = 10
+n2 = 2
+a = "Waldenir"
+d = 1.5
+
+#Comparando se as variaveis são iguais
+#print(n1 == n2)   
+
+#Comparar se as variaveis são diferentes
+#print(n1 != n2) 
+
+#Testar se a variavel n1 é maior que n2
+#print(n1 > n2) 
+
+#Testar se a variavel n1 é menor que n2
+#print(n1 < n2)
+
+#testar se a variavel n1 é maior ou igual que n2
+#print(n1 >= n2)
+
+#Testar se a variavel n1 é menor ou igual que n2
+#print(n1 <= n2)
+
+#testar se o dobro da variavel n1 é maior ou iqual a n2
+#print(2*n1 >= n2)
+
+#Testar a podenciação de um número
+#print(2**3)
+
+#multiplicação 
+#print(n1 * n2)
+
+#Divisão Quociente
+#print(n1 / n2)
+
+#Divisão resto
+#print(n1 % n2)
+
+#Soma
+#print(n1 + n2)
+
+#Subtração
+#print(n1 - n2)
+
+#Equação
+#print(n2 ** (2+1) + n1 % 3)
+
+#verificar tipo de variavel
+print(type(d))
